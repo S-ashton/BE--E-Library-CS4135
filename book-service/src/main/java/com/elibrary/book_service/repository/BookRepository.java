602d790be10e4bc5db1d8cc62023c1ddff3b2503
Katie Purser <22345477@studentmail.ul.ie>
2026-04-07 20:01:52 +0100

    First repository method

---
+++ book-service/src/main/java/com/elibrary/book_service/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.elibrary.book_service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.elibrary.book_service.model.*;
+
+
+public interface BookRepository extends JpaRepository<Book, Integer>{
+    boolean existsByTitleAndAuthorAndYearAndLanguage(String title, String author, int year, Languages language);   
+}
