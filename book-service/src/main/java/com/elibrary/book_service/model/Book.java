7e9cf2803e3e92eb26dde27e4bd23144bfc13bff
Katie Purser <22345477@studentmail.ul.ie>
2026-04-09 00:43:49 +0100

    Added alternate initialisation, DTO method and ES func

--- book-service/src/main/java/com/elibrary/book_service/model/Book.java
+++ book-service/src/main/java/com/elibrary/book_service/model/Book.java
@@ -1,44 +1,57 @@
 package com.elibrary.book_service.model;
 
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import com.elibrary.book_service.dto.TitleResponseDTO;
+
+
 import jakarta.persistence.*;
 
 //TODO: Implement tags/keywords and stored procedure for copiesAvailable
 
 @Entity
 @Table(name = "books")
+@Document(indexName = "books")
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long book_id;
+    private Long id;
 
     @Column(nullable = false, unique = true)
+    @Field(type = FieldType.Text)
     private String title;
 
     @Column(nullable = false)
+    @Field(type = FieldType.Text)
     private String author;
 
     @Column(nullable = false)
+    @Field(type = FieldType.Text)
     private String description;
 
     @Column(nullable = false)
+    @Field(type = FieldType.Integer)
     private int yearPublished;
 
     @Enumerated(EnumType.STRING)
+    @Field(type = FieldType.Keyword)
     @Column(nullable = false)
     private Genre genre;
-
-    // tags/keywords
 
     @Lob
     @Column(name = "cover_image", columnDefinition = "BYTEA")
     private byte[] coverImage;
 
     @Enumerated(EnumType.STRING)
+    @Field(type = FieldType.Keyword)
     @Column(nullable = false)
     private Languages language;
 
-    // copiesAvailable (stored procedure)
+    @Column(nullable = false)
+    private int copiesAvailable;
 
 
 
@@ -46,7 +59,25 @@ public class Book {
     protected Book() {}
 
     public Book(String title, String author, String description, int yearPublished, Genre genre, byte[] coverImage, Languages language) {
-        this(title, author, description, yearPublished, genre, coverImage, language);
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.yearPublished = yearPublished;
+        this.genre = genre;
+        this.coverImage = coverImage;
+        this.language = language;
+        this.copiesAvailable = 1;
+    }
+
+    public Book(String title, String author, String description, int yearPublished, Genre genre, byte[] coverImage, Languages language, int copiesAvailable) {
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.yearPublished = yearPublished;
+        this.genre = genre;
+        this.coverImage = coverImage;
+        this.language = language;
+        this.copiesAvailable = copiesAvailable;
     }
 
     public Long getId() {
@@ -107,5 +138,19 @@ public class Book {
 
     public void setLanguage(Languages language){
         this.language = language;
+    }
+
+    public int getCopiesAvailable(){
+        return copiesAvailable;
+    }
+
+    public void setCopiesAvailable(int numCopies){
+        this.copiesAvailable = numCopies;
+    }
+
+    public TitleResponseDTO toDto (){
+        return new TitleResponseDTO(this.id, this.title, this.author, this.description,
+                                    this.yearPublished, this.genre, this.coverImage, 
+                                    this.language, this.copiesAvailable);
     }
 }
