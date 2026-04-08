811bf61ec5368e6c3f13eb485bd4ed5fd145194c
Katie Purser <22345477@studentmail.ul.ie>
2026-04-09 00:46:15 +0100

    Additional query added for Long id

--- book-service/src/main/java/com/elibrary/book_service/repository/BookRepository.java
+++ book-service/src/main/java/com/elibrary/book_service/repository/BookRepository.java
@@ -6,4 +6,6 @@ import com.elibrary.book_service.model.*;
 
 public interface BookRepository extends JpaRepository<Book, Integer>{
     boolean existsByTitleAndAuthorAndYearAndLanguage(String title, String author, int year, Languages language);   
+
+    Book findByIdLong(Long titleId);
 }
