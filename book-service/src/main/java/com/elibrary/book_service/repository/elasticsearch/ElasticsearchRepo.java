8b9414aee94f39efce5a3ab064ba3f26b4b2f242
Katie Purser <22345477@studentmail.ul.ie>
2026-04-10 11:08:43 +0100

    Renamed and moved for Docker

---
+++ book-service/src/main/java/com/elibrary/book_service/repository/elasticsearch/ElasticsearchRepo.java
@@ -0,0 +1,11 @@
+package com.elibrary.book_service.repository.elasticsearch;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import com.elibrary.book_service.model.*;
+
+
+public interface ElasticsearchRepo extends ElasticsearchRepository<Book, String> {
+
+    
+}
