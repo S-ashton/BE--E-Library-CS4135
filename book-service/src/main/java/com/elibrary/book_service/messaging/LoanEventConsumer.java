947c23a38f0b7517547adf193a22e38cd7446a55
Roisin Mitchell <21193762@studentmail.ul.ie>
2026-04-13 17:11:09 +0100

    fix(book-service): declare loan event queues and bindings
    book-service MessagingConfig was missing Queue and Binding beans after the previous commit removed them from loan-service. LoanEventConsumer was also subscribing to the exchange name instead of a queue, causing the RabbitMQ channel error on startup. Added the missing beans and two new queue name properties to config-repo/book-service.yaml.

--- book-service/src/main/java/com/elibrary/book_service/messaging/LoanEventConsumer.java
+++ book-service/src/main/java/com/elibrary/book_service/messaging/LoanEventConsumer.java
@@ -4,7 +4,6 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
-import org.springframework.amqp.core.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,14 +12,14 @@ public class LoanEventConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(LoanEventConsumer.class);
 
-   @RabbitListener(queues = "loan.events")
+    @RabbitListener(queues = "${loan.events.borrowed-queue}")
     public void handleBorrowed(LoanBorrowedEvent event,
                             @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
         log.info("Loan borrowed event received, routing key: {}", routingKey);
         // use event directly
     }
 
-    @RabbitListener(queues = "loan.events")
+    @RabbitListener(queues = "${loan.events.returned-queue}")
     public void handleReturned(LoanReturnedEvent event,
                             @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
         log.info("Loan returned event received, routing key: {}", routingKey);
