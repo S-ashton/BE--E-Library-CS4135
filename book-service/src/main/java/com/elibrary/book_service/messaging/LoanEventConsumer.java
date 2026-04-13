dc3e5fc19aa03f35538d095771d9c3bf8924f866
Katie Purser <22345477@studentmail.ul.ie>
2026-04-13 11:57:28 +0100

    Added event processing

--- book-service/src/main/java/com/elibrary/book_service/messaging/LoanEventConsumer.java
+++ book-service/src/main/java/com/elibrary/book_service/messaging/LoanEventConsumer.java
@@ -4,7 +4,10 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
-import org.springframework.amqp.core.Message;
+
+import com.elibrary.book_service.model.Status;
+import com.elibrary.book_service.service.BookService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,17 +16,25 @@ public class LoanEventConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(LoanEventConsumer.class);
 
+    private BookService bookService;
+
+    public LoanEventConsumer(BookService bookService){
+        this.bookService = bookService;
+    }
+
    @RabbitListener(queues = "loan.events")
     public void handleBorrowed(LoanBorrowedEvent event,
                             @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
         log.info("Loan borrowed event received, routing key: {}", routingKey);
-        // use event directly
+        Long copyId = event.getCopyId();
+        bookService.changeStatus(copyId, Status.ON_LOAN);
     }
 
     @RabbitListener(queues = "loan.events")
     public void handleBorrowed(LoanReturnedEvent event,
                             @Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String routingKey) {
         log.info("Loan borrowed event received, routing key: {}", routingKey);
-        // use event directly
+        Long copyId = event.getCopyId();
+        bookService.changeStatus(copyId, Status.AVAILABLE);
     }
 }
