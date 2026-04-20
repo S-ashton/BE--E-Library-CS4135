cffc4698ee343485e0b3846dd853448b36765abf
Roisin Mitchell <21193762@studentmail.ul.ie>
2026-04-20 20:50:43 +0100

    Added CommandLineRunner for resync of elasticsearch

--- book-service/src/main/java/com/elibrary/book_service/BookServiceApplication.java
+++ book-service/src/main/java/com/elibrary/book_service/BookServiceApplication.java
@@ -1,13 +1,22 @@
 package com.elibrary.book_service;
 
+import com.elibrary.book_service.repository.elasticsearch.ElasticsearchRepo;
+import com.elibrary.book_service.repository.jpa.TitleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookServiceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner syncElasticsearch(TitleRepository jpaRepo, ElasticsearchRepo esRepo) {
+		return args -> esRepo.saveAll(jpaRepo.findAll());
 	}
 
 }
